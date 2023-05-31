e9ae3e5453c5bb15fca0a7ee706222d1ea9d2d52
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-06-01 03:42:26 +0600

    autorization and jwt token api complete

--- index.js
+++ index.js
@@ -2,11 +2,46 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 // middlewear  
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const verifyJWT = (req, res, next) => {
+
+
+  const authorization = req.headers.authorization
+
+
+
+  if (!authorization) {
+
+    return res.status(403).send({ error: true, message: 'unauthorization author' })
+
+  }
+
+  const token = authorization.split(' ')[1]
+
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+
+
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorization err' })
+    }
+
+    req.decoded = decoded
+
+    next()
+
+  });
+
+}
 
 
 
@@ -36,36 +71,101 @@ async function run() {
 
 
 
+    // verify admin 
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' })
+      }
+      next()
+
+
+    }
+
+
+
+
+
+
+
+
+
+    //  jwt token method
+
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1hr'
+      })
+
+      res.send({ token })
+
+    })
+
+
+
+
+
     // users method 
 
-app.get('/users',async(req,res)=>{
-  const result = await usersCollection.find().toArray()
-  res.send(result)
-})
+
+    /**
+     * 0. do not show secure links to those who should not see the links 
+     * 1. use jwt token : verifyJWT
+     * 2 . use verifyAdmin middleWare 
+     * 
+     * 
+     */
+
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
 
 
     app.post('/users', async (req, res) => {
       const user = req.body
-      const query = {email : user.email}
+      const query = { email: user.email }
       const existingUser = await usersCollection.findOne(query)
-      if(existingUser){
-        return res.send({message : 'user already exists'})
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
       }
       const result = await usersCollection.insertOne(user)
       res.send(result)
     })
 
 
-    app.patch('/users/admin/:id',async(req,res)=>{
-      const id = req.params.id 
-      const filter = {_id : new ObjectId(id)}
+    // security layer : verifyJWT 
+
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      const result = { admin: user?.role === 'admin' }
+      res.send(result)
+    })
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
       const updateDoc = {
         $set: {
           role: 'admin'
         },
       };
 
-      const result = await usersCollection.updateOne(filter,updateDoc)
+      const result = await usersCollection.updateOne(filter, updateDoc)
       res.send(result)
 
     })
@@ -77,6 +177,22 @@ app.get('/users',async(req,res)=>{
       res.send(result)
     })
 
+
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body
+      const result = await menuCollection.insertOne(newItem)
+      res.send(result)
+    })
+
+
+
+    app.delete('/menu/:id',verifyJWT,verifyAdmin ,async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // review method 
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray()
@@ -85,12 +201,27 @@ app.get('/users',async(req,res)=>{
     })
 
 
+
     // carts method 
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
+
+
+
       const email = req.query.email
-      // console.log(email)
+      const decodedEmail = req.decoded.email
+
+
+      console.log(decodedEmail, 'decorded email ')
+
+      console.log(email, 'query email')
+
       if (!email) {
         return res.send([])
+      }
+
+      if (email !== decodedEmail) {
+        return res.status(401).send({ error: true, message: 'unauthorization email' })
+
       }
 
       const query = { email: email }
